be80ac9752e671217e0115975f0786b38732d894
Daniel Gómez Villegas <gomezvillegasdaniel@gmail.com>
2015-12-16 19:24:42 +0000

    [REF] openacademy dia 3: Adding sql constraints

--- model/openacademy_course.py
+++ model/openacademy_course.py
@@ -6,8 +6,17 @@ class Course(models.Model):
 
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
-    responsible_id = fields.Many2one('res.users',
-                     		     ondelete='set null',
-				     string="Responsible", index=True)
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string='Sessions')
-    
+
+    _sql_constraints = [
+                       ('name_description_check',
+                        'CHECK(name != description)',
+                        "The title of the course should not be the description",
+                       ),
+
+                       ('name_unique',
+                        'UNIQUE(name)',
+                        "The course title must be unique",
+                       ),
+    ]
