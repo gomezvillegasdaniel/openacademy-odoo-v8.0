901e07219d800da77174ba405d66831967a53ae3
Daniel Gómez Villegas <gomezvillegasdaniel@gmail.com>
2015-12-18 17:24:58 +0000

    [REF] openacademy dia 4: Adding ir.model.access.csv

--- __openerp__.py
+++ __openerp__.py
@@ -25,7 +25,7 @@
 	'view/openacademy_session_view.xml',
 	'view/partner_view.xml',
     'workflow/openacademy_session_workflow.xml',
-        #'security/ir.model.access.csv',
+    'security/ir.model.access.csv',
         #'templates.xml',
     ],
     # only loaded in demonstration mode
