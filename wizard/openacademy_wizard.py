68450c9f128b58ec65415d1b3bfeae2600f66389
Daniel Gómez Villegas <gomezvillegasdaniel@gmail.com>
2015-12-21 23:29:53 +0000

    [FIX] openenacademy dia 4: writing the correct code

--- wizard/openacademy_wizard.py
+++ wizard/openacademy_wizard.py
@@ -10,11 +10,12 @@ class Wizard(models.TransientModel):
         print "self._context", self._context
         return self.env['openacademy.session'].browse(self._context.get('active_ids'))
     
-    session_wiz_ids = fields.Many2one('openacademy.session', string='Session', required=True, default=_default_session)
+    session_wiz_ids = fields.Many2many('openacademy.session', string='Session', required=True, default=_default_session)
     attendee_wiz_ids = fields.Many2many('res.partner', string='Attendees')
     
     @api.multi
     def subscribe(self):
         for session_wiz_id in self.session_wiz_ids:
-            session_wiz_id.attendee_ids |= self.attendee_wiz_ids
+            session_wiz_id.attendee_ids |= self.attendee_wiz_ids.ids
+        #  openacademy_session.write( session_wiz_id, {'attendee_ids' : [0,6, [self.attendee_wiz_ids]] )
         return {}
