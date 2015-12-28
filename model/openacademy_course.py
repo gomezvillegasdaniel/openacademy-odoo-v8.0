b1c1f2ee91dbb077ce1f39db33b421c6a57cb81c
Daniel Gómez Villegas <gomezvillegasdaniel@gmail.com>
2015-12-28 02:04:37 +0000

    [REF] openacademy dia 5: Adding method '_' to python string

--- model/openacademy_course.py
+++ model/openacademy_course.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import api, models, fields
+from openerp import api, models, fields, _
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -12,12 +12,12 @@ class Course(models.Model):
     _sql_constraints = [
                        ('name_description_check',
                         'CHECK(name != description)',
-                        "The title of the course should not be the description",
+                        _("The title of the course should not be the description"),
                        ),
 
                        ('name_unique',
                         'UNIQUE(name)',
-                        "The course title must be unique",
+                        _("The course title must be unique"),
                        ),
     ]
     
@@ -27,9 +27,9 @@ class Course(models.Model):
         copied_count = self.search_count(
                                         [('name', '=like', u"Copy of {}%".format(self.name))])
         if not copied_count:
-            new_name = u"Copy of {}".format(self.name)
+            new_name = _(u"Copy of {}").format(self.name)
         else:
-            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
 
         default['name'] = new_name
         return super(Course, self).copy(default)
