8dbc6a643be0d22fd7b7f09c85f262bbc1f43ce6
Daniel Gómez Villegas <gomezvillegasdaniel@gmail.com>
2016-01-01 03:43:32 +0000

    [ADD] openacademy dia 5: Add basic report qweb

--- __openerp__.py
+++ __openerp__.py
@@ -28,6 +28,7 @@
     'workflow/openacademy_session_workflow.xml',
     'security/security.xml',
     'security/ir.model.access.csv',
+    'report/openacademy_session_report.xml',
         #'templates.xml',
     ],
     # only loaded in demonstration mode
